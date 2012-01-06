05d4bc6d94b84b34c24a24e71e21360754d3b2b5
Lin He <he9lin@gmail.com>
2012-01-06 06:08:59 -0800

    Add mail function with simple error checking

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,16 @@
 class HomeController < ApplicationController
+  respond_to :js, :html
+  
   def index
   end
   
   def mail
-    MessageMailer.from(params[:name], params[:email], params[:content]).deliver
+    if params[:username].present? && params[:email].present? && params[:content].present?
+      MessageMailer.from(params[:username], params[:email], params[:content]).deliver
+      flash.now.notice = t('mail.sent')
+    else
+      flash.now.notice = t('mail.missing')
+      render :error
+    end
   end
 end
